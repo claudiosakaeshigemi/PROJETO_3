b491e2316545bed85e710f59a9ec0874a0bfa837
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-04 19:06:36 -0300

    Aula 57
    Ajax Request with Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
+
+
 const API_KEY ='165d662266f76c0853d923fc7131264c';
+//  ES6 sintax ` ${API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${CITY},br`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER
+        
+    };
+}
