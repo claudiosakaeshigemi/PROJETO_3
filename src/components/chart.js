282040e1346c98784652ac77327fe4f7553d50ae
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-10 19:57:57 -0300

    aula 65.
    Making a Reusable Chart Component

---
+++ src/components/chart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Sparklines, SparklinesLine} from 'react-sparklines';
+
+export default (props) => {
+    return ( 
+        <div>
+            <Sparklines height={100} width={150} data = {props.data} >
+                <SparklinesLine color ={props.color}/>
+            </Sparklines>
+
+        </div>
+    );
+}
