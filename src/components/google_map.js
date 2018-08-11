1a854848effe0a0be8851735edfa22c672a7c77f
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-11 11:15:12 -0300

    Aula 68
    Google Maps Integration Continued

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -4,7 +4,7 @@ class GoogleMap extends Component {
 
 
     componentDidMount() {
-        new GoogleMap.maps.Map(this.refs.map, {
+        new google.maps.Map(this.refs.map, {
             zoom: 12,
             center: {
                 lat: this.props.lat,
